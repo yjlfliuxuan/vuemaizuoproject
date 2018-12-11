4c7657849cda6ee6c6c0a018643a7c5254451925
yjlfliuxuan <2226802518@qq.com>
2018-12-11 17:38:57 +0800

    单文件组件与.eslintrc更改

--- maizuoforce/src/main.js
+++ maizuoforce/src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 整个前端项目的入口js文件
 import Vue from 'vue'
+// 单文件组件（.vue）一个文件就是一个组件 import App from './App.vue'  .vue可以省略n
 import App from './App'
-
+// 去除vue开发时的浏览器提示
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
