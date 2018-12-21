a0aa9c7aa32d0f1c0eb55b4075a7d7e6d67438d6
yjlfliuxuan <2226802518@qq.com>
2018-12-21 18:01:44 +0800

    项目优化

--- maizuoforce/src/views/citys.vue
+++ maizuoforce/src/views/citys.vue
@@ -49,371 +49,21 @@
         </div>
       </div>
       <div
+        v-for="(item,index) in citys"
+        :key="index"
         class="znletter"
-        id="letterA"
+        :id="'letter'+item.word"
       >
-        <h3>A</h3>
-        <div class="gps1 citysA">
+        <h3>{{item.word}}</h3>
+        <div class="gps1">
           <router-link
             :to="{path:'/films/nowPlaying'}"
             tag="div"
-            @click.stop.native="change(item)"
+            @click.stop.native="change(items.name)"
             class="gpsdw1"
-            v-for="(item,index) in citysA"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterB"
-      >
-        <h3>B</h3>
-        <div class="gps1 citysB">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysB"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterC"
-      >
-        <h3>C</h3>
-        <div class="gps1 citysC">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysC"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterD"
-      >
-        <h3>D</h3>
-        <div class="gps1 citysD">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysD"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterE"
-      >
-        <h3>E</h3>
-        <div class="gps1 citysE">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysE"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterF"
-      >
-        <h3>F</h3>
-        <div class="gps1 citysF">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysF"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterG"
-      >
-        <h3>G</h3>
-        <div class="gps1 citysG">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysG"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterH"
-      >
-        <h3>H</h3>
-        <div class="gps1 citysH">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysH"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterI"
-      >
-        <h3>I</h3>
-        <div class="gps1 citysI">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysI"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterJ"
-      >
-        <h3>J</h3>
-        <div class="gps1 citysJ">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysJ"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterK"
-      >
-        <h3>K</h3>
-        <div class="gps1 citysK">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysK"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterL"
-      >
-        <h3>L</h3>
-        <div class="gps1 citysL">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysL"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterM"
-      >
-        <h3>M</h3>
-        <div class="gps1 citysM">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysM"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterN"
-      >
-        <h3>N</h3>
-        <div class="gps1 citysN">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysN"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterP"
-      >
-        <h3>P</h3>
-        <div class="gps1 citysP">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysP"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterQ"
-      >
-        <h3>Q</h3>
-        <div class="gps1 citysQ">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysQ"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterR"
-      >
-        <h3>R</h3>
-        <div class="gps1 citysR">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysR"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterS"
-      >
-        <h3>S</h3>
-        <div class="gps1 citysS">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysS"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterT"
-      >
-        <h3>T</h3>
-        <div class="gps1 citysT">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysT"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterW"
-      >
-        <h3>W</h3>
-        <div class="gps1 citysW">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysW"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterX"
-      >
-        <h3>X</h3>
-        <div class="gps1 citysX">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysX"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterY"
-      >
-        <h3>Y</h3>
-        <div class="gps1 citysY">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysY"
-            :key="index"
-          >{{item}}</router-link>
-        </div>
-      </div>
-      <div
-        class="znletter"
-        id="letterZ"
-      >
-        <h3>Z</h3>
-        <div class="gps1 citysZ">
-          <router-link
-            :to="{path:'/films/nowPlaying'}"
-            tag="div"
-            @click.stop.native="change(item)"
-            class="gpsdw1"
-            v-for="(item,index) in citysZ"
-            :key="index"
-          >{{item}}</router-link>
+            v-for="(items,indexs) in item.cityname"
+            :key="indexs"
+          >{{items.name}}</router-link>
         </div>
       </div>
     </div>
@@ -440,29 +90,7 @@ export default {
       citysname: '',
       letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"],
       hotcitys: [],
-      citysA: [],
-      citysB: [],
-      citysC: [],
-      citysD: [],
-      citysE: [],
-      citysF: [],
-      citysG: [],
-      citysH: [],
-      citysI: [],
-      citysJ: [],
-      citysK: [],
-      citysL: [],
-      citysM: [],
-      citysN: [],
-      citysP: [],
-      citysQ: [],
-      citysR: [],
-      citysS: [],
-      citysT: [],
-      citysW: [],
-      citysX: [],
-      citysY: [],
-      citysZ: []
+      citys: []
     }
   },
   components: {
@@ -510,81 +138,21 @@ export default {
         if (item.isHot === 1) {
           this.hotcitys.push(item.name);
         }
-        var firstletter = item.pinyin.charAt(0).toUpperCase();
-        switch (firstletter) {
-          case "A":
-            this.citysA.push(item.name);
-            break;
-          case "B":
-            this.citysB.push(item.name);
-            break;
-          case "C":
-            this.citysC.push(item.name);
-            break;
-          case "D":
-            this.citysD.push(item.name);
-            break;
-          case "E":
-            this.citysE.push(item.name);
-            break;
-          case "F":
-            this.citysF.push(item.name);
-            break;
-          case "G":
-            this.citysG.push(item.name);
-            break;
-          case "H":
-            this.citysH.push(item.name);
-            break;
-          case "I":
-            this.citysI.push(item.name);
-            break;
-          case "J":
-            this.citysJ.push(item.name);
-            break;
-          case "K":
-            this.citysK.push(item.name);
-            break;
-          case "L":
-            this.citysL.push(item.name);
-            break;
-          case "M":
-            this.citysM.push(item.name);
-            break;
-          case "N":
-            this.citysN.push(item.name);
-            break;
-          case "P":
-            this.citysP.push(item.name);
-            break;
-          case "Q":
-            this.citysQ.push(item.name);
-            break;
-          case "R":
-            this.citysR.push(item.name);
-            break;
-          case "S":
-            this.citysS.push(item.name);
-            break;
-          case "T":
-            this.citysT.push(item.name);
-            break;
-          case "W":
-            this.citysW.push(item.name);
-            break;
-          case "X":
-            this.citysX.push(item.name);
-            break;
-          case "Y":
-            this.citysY.push(item.name);
-            break;
-          case "Z":
-            this.citysZ.push(item.name);
-            break;
-          default:
-            break;
-        }
       });
+      for(var i=0;i<this.letters.length;i++){
+         /* eslint-disable */
+         var firstletterreg=new RegExp("^"+this.letters[i]);
+          var temp=this.arr.filter(item=>{
+           if(firstletterreg.test(item.pinyin.toUpperCase())){
+             return item.name;
+          }
+        })
+        this.citys.push({
+            word: this.letters[i],
+            cityname: temp
+        })
+      }
+      console.log(this.citys);
     })
   }
 }
